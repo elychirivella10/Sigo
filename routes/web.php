46a6a49a29cf9fd9c23695e8c44e49f6d8c83c1c
gfrancojf <gfrancojf@gmail.com>
2022-07-27 15:19:56 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'], function () {
 
-Route::get('/', function () {
-    return view('home');
-});
-});
-Auth::routes();
-
+Route::get('/', function () { return view('home');});
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+});
+
+
